4b6eb99a8abce1262459728e67910cc79959d605
preethasri <preethasrinivasan619@gmail.com>
2021-05-30 20:19:10 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textinput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function clickHandler() {
+    var inputText = textinput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+
+};
+btnTranslate.addEventListener("click", clickHandler)
